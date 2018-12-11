2e94f5475b9c3ddd9aa0ff79e6e0d8d094688f8e
M Rivaldo <43290649+Rivaldz@users.noreply.github.com>
2018-12-11 12:35:14 +0000

    Mengubah File koneksi.php

--- jadi/koneksi.php
+++ jadi/koneksi.php
@@ -4,9 +4,4 @@
 		if (!$Open){
 		die ("Koneksi ke Engine MySQL Gagal !<br>");
 		}
-//koneksi ke database
-	// $Koneksi = mysqli_select_db("koperasi_new");
-	// 	if (!$Koneksi){
-	// 	die ("Koneksi ke Database Gagal !");
-	// 	}
 ?>
